060206a7a6da7d934aa5240b15c14d0a055bd81f
matthewFisher <m.r.f.48215@gmail.com>
2019-05-06 20:52:37 +0900

    Initial commit.

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+
+import Layout from '../components/layout';
+
+const LandingPage = () => (
+  <Fragment>
+    <h1>Landing</h1>
+    {console.log(process.env.REACT_APP_API_KEY, "hredkfhdsf")}
+    <p>
+      The Landing Page is open to everyone, even though the user isn't
+      signed in.
+    </p>
+  </Fragment>
+);
+
+export default () => (
+  <Layout>
+    <LandingPage />
+  </Layout>
+);
