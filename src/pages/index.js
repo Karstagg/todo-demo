faec4de6f86fa117e16e1c2e17cae7506d364faf
matthewFisher <m.r.f.48215@gmail.com>
2019-05-08 18:36:00 +0900

    trying to fix css

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const LandingPage = () => (
             <CardTitle> <h2>Todo List</h2></CardTitle>
             <CardText><h5>This is a demo todo app built by Matthew Fisher using Gatsby.js/react, Firebase, and Netlify.</h5></CardText>
             <CardText>To get started please visit the sign in page to sign in or sign up using e-mail or Google</CardText>
-            <Link to='./signin'><Button className='card-button' color='primary'>Get Started</Button></Link>
+            <CardText><Link to='./signin'><Button className='card-button' color='primary'>Get Started</Button></Link></CardText>
           </Card>
         </Col>
       </Row>
